b41afb60a2e6195bfd97267a9cf7e9a405ac25c9
DaherRivas <daherrivas7@gmail.com>
2021-12-14 19:21:16 -0300

    Se agrega el Proyecto

---
+++ sweetalert.js
@@ -0,0 +1,13 @@
+Swal.fire ({
+    title: 'Bienvenido!!',
+    html: '<b class="azul">Antes de seguir con la visita, por favor registrarse:</b>',
+    icon: 'info',
+    input: 'text',
+    inputPlaceHolder: 'Nombre',
+    inputValue: 'Nombre',
+    footer: '<span class="rojo">*No se almacenarán los datos ingresados*</span>',
+    confirmButtonText: 'OK',
+    confirmButtonColor: 'blue',
+    showCancelButton: 'true',
+    cancelButtonColor: 'red'
+  })
